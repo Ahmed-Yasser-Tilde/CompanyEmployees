d4f0428cec5456d2f282d1e8ca35a2eca23155e6
Ahmed <ahmed.yasser@tildetech.ae>
2025-04-06 15:32:04 +0200

    Add project files.

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,44 @@
+using CompanyEmployees.Extensions;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(),
+"/nlog.config"));
+
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+// Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+
+/*app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method");
+    await context.Response.WriteAsync("Hello from the middleware component.");
+});*/
+
+app.MapControllers();
+
+app.Run();
